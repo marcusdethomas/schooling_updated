c1c79bba161ddf2ca5b97f75511586569a303aba
Marcus Thomas <marcusdethomas@yahoo.com>
2022-11-08 21:00:13 -0700

    fixed pic view

--- schooling/home/homeschool_app/urls.py
+++ schooling/home/homeschool_app/urls.py
@@ -13,18 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
+from django.urls import path
+from ..home import views
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  #path('', include('django.contrib.auth.urls')),
-                  path('', views.index)
-              ] + static(settings.MEDIA_URL,
-                         document_root=settings.MEDIA_ROOT)
+                  path('/', views.index()),
+                  # path('', include('django.contrib.auth.urls'))
+                  # path('/', views.index())
+              ] #+ static(settings.MEDIA_URL,
+                #         document_root=settings.MEDIA_ROOT)
 ''''
 if settings.DEBUG:
     urlpatterns + static(settings.MEDIA_URL,
