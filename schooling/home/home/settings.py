4c6fe0ca9d35c71156a014dbc47d373ba4898d9b
Marcus Thomas <marcusdethomas@yahoo.com>
2022-11-08 18:28:20 -0700

    commit

--- schooling/home/home/settings.py
+++ schooling/home/home/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3*14squd$m7c#r$co(jgf8l-p_q17g*4--&c%(-oib@y94gb$9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_URL = '/images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- schooling/home/home/urls.py
+++ schooling/home/home/urls.py
@@ -15,10 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-#from home.homeschool_app import views
-#from ..homeschool_app import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+# from home.homeschool_app import views
+# from ..homeschool_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('/', views.index())
+    # path('/', views.index())
 ]
+if settings.DEBUG:
+    urlpatterns + static(settings.MEDIA_URL,
+                         document_root=settings.MEDIA_ROOT)
+    print("Base Directory: ", settings.BASE_DIR)
+    print("Media Root: ", settings.MEDIA_ROOT)
+    print("Media Url: ", settings.MEDIA_URL)
