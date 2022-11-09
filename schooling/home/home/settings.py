931f291cd302cc22ff81e1ce4d077f944051cefc
Marcus Thomas <marcusdethomas@yahoo.com>
2022-11-08 21:51:54 -0700

    fixed pic view

--- schooling/home/home/settings.py
+++ schooling/home/home/settings.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3*14squd$m7c#r$co(jgf8l-p_q17g*4--&c%(-oib@y94gb$9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homeschool_app'
+    'homeschool_app',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR))
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    #STATIC_ROOT,
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'home/images')
+MEDIA_URL = '/images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
