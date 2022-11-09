7c4e8b02c708c5108a85dbaba415d8bb0065e3e2
Marcus Thomas <marcusdethomas@yahoo.com>
2022-11-08 20:05:03 -0700

    commit

--- schooling/home/home/settings.py
+++ schooling/home/home/settings.py
@@ -13,7 +13,7 @@ import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,7 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR))
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    #STATIC_ROOT,
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'home/images')
 MEDIA_URL = '/images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- schooling/home/home/urls.py
+++ schooling/home/home/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -22,12 +22,15 @@ from django.conf.urls.static import static
 # from ..homeschool_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('/', views.index())
-]
+                  path('admin/', admin.site.urls),
+                  # path('', include('django.contrib.auth.urls'))
+                  # path('/', views.index())
+              ] + static(settings.MEDIA_URL,
+                         document_root=settings.MEDIA_ROOT)
+''''
 if settings.DEBUG:
     urlpatterns + static(settings.MEDIA_URL,
                          document_root=settings.MEDIA_ROOT)
     print("Base Directory: ", settings.BASE_DIR)
     print("Media Root: ", settings.MEDIA_ROOT)
-    print("Media Url: ", settings.MEDIA_URL)
+'''
